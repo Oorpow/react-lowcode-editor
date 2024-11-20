347b9c10793e8f6271bec54ce8a7c8111f423c4c
Oorpow <309754513@qq.com>
2024-11-20 15:20:39 +0800

    docs: 📃 补充useMaterialDrop的部分注释

--- src/hooks/useMaterialDrop.ts
+++ src/hooks/useMaterialDrop.ts
@@ -2,6 +2,12 @@ import { useDrop } from "react-dnd"
 import { useComponentConfigStore } from "../editor/stores/component-config"
 import { useComponentsStore } from "../editor/stores/components"
 
+/**
+ * 物料区组件Drop到编辑区的逻辑
+ * @param accept 接收的组件类型数组
+ * @param id 
+ * @returns 
+ */
 export const useMaterialDrop = (accept: string[], id: number) => {
     const { addComponent } = useComponentsStore()
     const { componentConfig } = useComponentConfigStore()
@@ -10,6 +16,7 @@ export const useMaterialDrop = (accept: string[], id: number) => {
         return {
             accept,
             drop: (item: { type: string }, monitor) => {
+                // 防止drop重复执行
                 if (monitor.didDrop()) return
 
                 const props = componentConfig[item.type].defaultProps
