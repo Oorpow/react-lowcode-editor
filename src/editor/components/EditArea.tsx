7e25646fad4a3a7de8da97c20b45412280abc9b6
Oorpow <309754513@qq.com>
2024-11-26 14:50:49 +0800

    feat: 鼠标hover到编辑区组件时会展示对应的组件名

--- src/editor/components/EditArea.tsx
+++ src/editor/components/EditArea.tsx
@@ -1,46 +1,95 @@
-import { createElement, ReactNode, useEffect } from "react";
-import { Component, useComponentsStore } from "../stores/components";
-import { useComponentConfigStore } from "../stores/component-config";
+import {
+	createElement,
+	MouseEventHandler,
+	ReactNode,
+	useEffect,
+	useState,
+} from 'react';
+import { Component, useComponentsStore } from '../stores/components';
+import { useComponentConfigStore } from '../stores/component-config';
+import HoverMask from './HoverMask';
 
 function EditArea() {
-	const { components, addComponent } = useComponentsStore()
-	const { componentConfig } = useComponentConfigStore()
+	const [hoverComponentId, setHoverComponentId] = useState<number>();
+
+	const { components, addComponent } = useComponentsStore();
+	const { componentConfig } = useComponentConfigStore();
 
 	useEffect(() => {
-		addComponent({
-			id: 2,
-			name: 'Container',
-			props: {},
-			children: [],
-		}, 1)
-	}, [])
+		addComponent(
+			{
+				id: 2,
+				name: 'Container',
+				props: {},
+				children: [],
+			},
+			1
+		);
+	}, []);
 
 	function renderComponents(components: Component[]): ReactNode {
 		return components.map((comp: Component) => {
-			const config = componentConfig?.[comp.name]
+			const config = componentConfig?.[comp.name];
 
 			if (!config.component) {
-				return null
+				return null;
 			}
 
-			return createElement(config.component, {
-				key: comp.id,
-				id: comp.id,
-				name: comp.name,
-				...config.defaultProps,
-				...comp.props
-			}, renderComponents(comp.children || []))
-		})
+			return createElement(
+				config.component,
+				{
+					key: comp.id,
+					id: comp.id,
+					name: comp.name,
+					...config.defaultProps,
+					...comp.props,
+				},
+				renderComponents(comp.children || [])
+			);
+		});
 	}
 
-	return <div className="h-full">
-		{/* <pre>
-			{
-				JSON.stringify(components, null, 2)
+	// 获取data-component-id
+	const handleMouseOver: MouseEventHandler = (e) => {
+		// 无遍历获取data-component-id方式
+		const target = (e.target as HTMLElement).closest('[data-component-id]')
+		if (target) {
+			const componentId = target.getAttribute('data-component-id');
+			if (componentId) {
+				setHoverComponentId(+componentId);
 			}
-		</pre> */}
-		{ renderComponents(components) }
-	</div>;
+		}
+		// const path = e.nativeEvent.composedPath();
+		// for (let i = 0; i < path.length; i += 1) {
+		// 	const el = path[i] as HTMLElement;
+		// 	// 取出material组件里的自定义属性data-component-id
+		// 	const componentId = el.dataset.componentId;
+		// 	if (componentId) {
+		// 		setHoverComponentId(+componentId);
+		// 		return;
+		// 	}
+		// }
+	};
+
+	return (
+		<div
+			className="h-full edit-area"
+			onMouseOver={handleMouseOver}
+			onMouseLeave={() => {
+				setHoverComponentId(undefined);
+			}}
+		>
+			{hoverComponentId && (
+				<HoverMask
+					componentId={hoverComponentId}
+					containerClassName="edit-area"
+					portalWrapperClassName="portal-wrapper"
+				/>
+			)}
+			{renderComponents(components)}
+			<div className='portal-wrapper'></div>
+		</div>
+	);
 }
 
 export default EditArea;
---
+++ src/editor/components/HoverMask/index.tsx
@@ -0,0 +1,110 @@
+import {
+	getComponentById,
+	useComponentsStore,
+} from '@/editor/stores/components';
+import { useEffect, useMemo, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+interface HoverMaskProps {
+	portalWrapperClassName: string;
+	containerClassName: string;
+	componentId: number;
+}
+
+function HoverMask({
+	containerClassName,
+	portalWrapperClassName,
+	componentId,
+}: HoverMaskProps) {
+	const [position, setPosition] = useState({
+		left: 0,
+		top: 0,
+		width: 0,
+		height: 0,
+		labelTop: 0,
+		labelLeft: 0,
+	});
+
+	const { components } = useComponentsStore();
+
+	useEffect(() => {
+		updatePosition();
+	}, [componentId]);
+
+	function updatePosition() {
+		if (!componentId) return;
+
+		const container = document.querySelector(`.${containerClassName}`);
+		if (!container) return;
+
+		const node = document.querySelector(
+			`[data-component-id="${componentId}"]`
+		);
+		if (!node) return;
+
+		const { top, left, width, height } = node.getBoundingClientRect();
+		const { top: containerTop, left: containerLeft } =
+			container.getBoundingClientRect();
+
+		let labelTop = top - containerTop + container.scrollTop;
+		const labelLeft = left - containerLeft + width;
+
+		if (labelTop <= 0) {
+			labelTop -= -20;
+		}
+
+		setPosition({
+			top: top - containerTop + container.scrollTop,
+			left: left - containerLeft + container.scrollTop,
+			width,
+			height,
+			labelTop,
+			labelLeft,
+		});
+	}
+
+	const el = useMemo(() => {
+		return document.querySelector(`.${portalWrapperClassName}`)!;
+	}, []);
+
+	const curComponent = useMemo(() => {
+		return getComponentById(componentId, components);
+	}, [componentId]);
+
+	return createPortal(
+		<>
+			<div
+				className="box-border absolute border-2 border-blue-400 pointer-events-none "
+				style={{
+					left: position.left,
+					top: position.top,
+					backgroundColor: 'rgba(0, 0, 255, 0.05)',
+					width: position.width,
+					height: position.height,
+					zIndex: 12,
+				}}
+			/>
+			<div
+				className="absolute -translate-x-full -translate-y-full "
+				style={{
+					left: position.labelLeft,
+					top: position.labelTop,
+					zIndex: 13,
+					display:
+						!position.width || position.width < 10
+							? 'none'
+							: 'inline',
+				}}
+			>
+				<div
+                    className='px-2 text-sm text-white bg-blue-500 border-4 cursor-pointer whitespace-nowrap'
+				>
+					{curComponent?.name}
+				</div>
+			</div>
+		</>,
+		el
+	);
+}
+
+export default HoverMask;
