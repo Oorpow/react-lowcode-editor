40b8fdfa964835b2185bf296862d755439a5fd53
Oorpow <309754513@qq.com>
2024-12-12 09:58:34 +0800

    feat: MonacoEditor组件实现json源码展示

--- src/editor/components/MaterialWrapper/index.tsx
+++ src/editor/components/MaterialWrapper/index.tsx
@@ -1,29 +1,25 @@
-import { FunctionComponent, useState } from "react";
-import { Segmented } from "antd";
-import Material from "../Material";
-import Outline from "../Outline";
-import Source from "../Source";
+import { FunctionComponent, useState } from 'react';
+import { Segmented } from 'antd';
+import Material from '../Material';
+import Outline from '../Outline';
+import Source from '../Source';
 
-interface MaterialWrapperProps {
-    
-}
+interface MaterialWrapperProps {}
 
 const MaterialWrapper: FunctionComponent<MaterialWrapperProps> = () => {
-    const [key, setKey] = useState('物料')
-    const segmentedOptions = ['物料', '大纲', '源码']
+	const [key, setKey] = useState('物料');
+	const segmentedOptions = ['物料', '大纲', '源码'];
 
-    return <div className="pt-5">
-        <Segmented onChange={setKey} block options={segmentedOptions} />
-        {
-            key === '物料' && <Material />
-        }
-        {
-            key === '大纲' && <Outline />
-        }
-        {
-            key === '源码' && <Source />
-        }
-    </div>;
-}
+	return (
+		<div>
+			<Segmented onChange={setKey} block options={segmentedOptions} />
+			<div className="pt-5 h-[calc(100vh-60px-30px-20px)]">
+				{key === '物料' && <Material />}
+				{key === '大纲' && <Outline />}
+				{key === '源码' && <Source />}
+			</div>
+		</div>
+	);
+};
 
 export default MaterialWrapper;
--- src/editor/components/Source/index.tsx
+++ src/editor/components/Source/index.tsx
@@ -1,13 +1,39 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent } from 'react';
+import MonacoEditor, { OnMount } from '@monaco-editor/react';
+import { useComponentsStore } from '@/editor/stores/components';
 
-interface SourceProps {
-    
-}
- 
+interface SourceProps {}
+
 const Source: FunctionComponent<SourceProps> = () => {
-    return <div>
-        source
-    </div>;
-}
- 
+	const { components } = useComponentsStore();
+
+	const handleEditorMount: OnMount = (editor, monaco) => {
+		// 支持使用cmd + J来进行格式化
+		editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyJ, () => {
+			editor.getAction('editor.action.formatDocument')?.run();
+		});
+	};
+
+	return (
+		<MonacoEditor
+            height='100%'
+			language="json"
+			path="components.json"
+			value={JSON.stringify(components, null, 2)}
+			onMount={handleEditorMount}
+			options={{
+				fontSize: 14,
+				scrollBeyondLastLine: false,
+				minimap: {
+					enabled: false,
+				},
+				scrollbar: {
+					verticalScrollbarSize: 6,
+					horizontalScrollbarSize: 6,
+				},
+			}}
+		/>
+	);
+};
+
 export default Source;
